309e45e3c9f20f67958cf3d2a4dbf68531b5519a
Oran Dennison <oran@dennison.io>
2022-07-04 13:31:27 -0800

    add Bluetooth selection for slots 5 and 6. may still need to add ZMK support for 6th slot.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,7 +8,7 @@
 U_NP,              U_NP,              &lt U_MEDIA ESC,   &lt U_NAV SPC,     &lt U_MOUSE TAB,   &lt U_SYM RET,     &lt U_NUM BSPC,    &lt U_FUN DEL,     U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MEDIA \
-&bootloader,       &to U_TAP,         &to U_EXTRA,       &to U_BASE,        U_NA,              U_NA,              &kp F14,           &kp F15,          &u_rgb_sai,        &u_rgb_bri,        \
+&bootloader,       &to U_TAP,         &to U_EXTRA,       &to U_BASE,        U_NA,              U_NA,              &kp F14,           &kp F15,          &u_bt_sel_4,        &u_bt_sel_5,        \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_ep_tog,         &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
 U_NA,              &kp RALT,          &to U_FUN,         &to U_MEDIA,       U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
