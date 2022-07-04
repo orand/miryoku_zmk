25da18480d6d19f2e819fc35923a8ce213d9e293
Oran Dennison <oran@dennison.io>
2022-07-04 11:57:16 -0800

    add custom media layer config which adds brightness controls to top index/middle

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_MEDIA \
+&bootloader,       &to U_TAP,         &to U_EXTRA,       &to U_BASE,        U_NA,              U_NA,              C_BRI_DN,          C_BRI_UP,          &u_rgb_sai,        &u_rgb_bri,        \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_ep_tog,         &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
+U_NA,              &kp RALT,          &to U_FUN,         &to U_MEDIA,       U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
