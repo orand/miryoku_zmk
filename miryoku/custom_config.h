4b16d7ead6623b940f1daa3f2e0bd98e86ad3a9a
Oran Dennison <oran@dennison.io>
2022-07-20 14:45:58 -0800

    clone the base layer for troubleshooting

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -15,9 +15,9 @@ U_NA,              &kp RALT,          &to U_FUN,         &to U_MEDIA,       U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
 
 #define MIRYOKU_LAYER_HFN \
-U_NA, 	U_NA, 	U_NA, 	&U_NA, 	U_NA,    	U_NA, 	U_NA, 	U_NA, 	U_NA,  U_NA, \
-U_NA, 	U_NA, 	U_NA, 	U_NA, 	U_NA,    	U_NA, 	U_NA, 	U_NA, 	U_NA, U_NA, \
-U_NA, 						U_NA, 						U_NA, 						U_NA, 						U_NA,    						U_NA, 	U_NA, 	U_NA, 	U_NA, U_NA, \
-U_NP,              			U_NP,              			U_NA,              			U_NA,              			U_NA,              				U_NA,       U_NA,       U_NA,       U_NP,                      U_NP
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&hm LGUI A,        &hm LALT R,        &hm LCTRL S,       &hm LSHFT T, &hm LC(LG(LS(LALT))) G, &hm LC(LG(LS(LALT))) M,             &hm LSHFT N,       &hm LCTRL E,       &hm LALT I,        &hm LGUI O,        \
+&lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hm RALT DOT,      &lt U_BUTTON SLASH,\
+U_NP,              U_NP,              &lt U_MEDIA ESC,   &lt U_NAV SPC,     &lt U_MOUSE TAB,   &lt U_SYM RET,     &lt U_NUM BSPC,    &lt U_FUN DEL,     U_NP,              U_NP
 
 #define MIRYOKU_LAYERMAPPING_HFN MIRYOKU_MAPPING
